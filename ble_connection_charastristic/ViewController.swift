e3f12e65375171f5d742398c0e01fdea9eee1d76
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-02-05 16:55:20 +0900

    Initial Commit

---
+++ ble_connection_charastristic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble_connection_charastristic
+//
+//  Created by Kiichi  on 2019/02/05.
+//  Copyright © 2019 Kiichi Sugihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ble_connection_charastristicTests/ble_connection_charastristicTests.swift
@@ -0,0 +1,34 @@
+//
+//  ble_connection_charastristicTests.swift
+//  ble_connection_charastristicTests
+//
+//  Created by Kiichi  on 2019/02/05.
+//  Copyright © 2019 Kiichi Sugihara. All rights reserved.
+//
+
+import XCTest
+@testable import ble_connection_charastristic
+
+class ble_connection_charastristicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
